ae6869f1c6ae87c52c823fff4dd1a3eef1439318
anthonysutardja <anthony.sutardja@gmail.com>
2013-11-19 21:05:41 -0800

    added initial probabilities and Theta class

--- mrca.py
+++ mrca.py
@@ -6,6 +6,34 @@ HMM implementation for finding most recent common ancestor.
 Anthony Sutardja
 Kevin Tee
 """
+from collections import namedtuple
+
+# Where m: P(Q=k), a: transition prob, e: emission prob
+Theta = namedtuple('Theta', ['m', 'a', 'e'])
+
+INITIAL_MU_PARAMS = Theta(
+    # Marginal Probabilities
+    {
+        1: 0.603154,
+        2: 0.357419,
+        3: 0.0388879,
+        4: 0.000539295
+    },
+    # Transition Probabilities
+    {
+        1: {1: 0.999916, 2: 0.0000760902, 3: 8.27877e-6, 4: 1.14809e-7},
+        2: {1: 0.000128404, 2: 0.999786, 3: 0.0000848889, 4: 1.17723e-6},
+        3: {1: 0.000128404, 2: 0.000780214, 3: 0.999068, 4: 0.0000235507},
+        4: {1: 0.000128404, 2: 0.000780214, 3: 0.00169821, 4: 0.997393},
+    },
+    # Emission Probabilities
+    {
+        1: {'I': 0.999608, 'D': 0.000391695},
+        2: {'I': 0.998334, 'D': 0.00166636},
+        3: {'I': 0.995844, 'D': 0.00415567},
+        4: {'I': 0.991548, 'D': 0.008452},
+    }
+)
 
 
 def read_fasta_sequences_to_str(filename):
@@ -40,26 +68,26 @@ def observe_differences(seq1, seq2):
 
     Returns a list of difference observations.
     >>> observe_differences('hello', 'jello')
-    [0, 1, 1, 1, 1]
+    ['D', 'I', 'I', 'I', 'I']
     >>> observe_differences('abc', 'abcdefg')
-    [1, 1, 1, 0, 0, 0, 0]
+    ['I', 'I', 'I', 'D', 'D', 'D', 'D']
     """
     i, j = 0, 0
     observations = []
     while i < len(seq1) and j < len(seq2):
         if seq1[i] == seq2[j]:
-            observations.append(1)
+            observations.append('I')
         else:
-            observations.append(0)
+            observations.append('D')
         i += 1
         j += 1
 
     # process tails
     while i < len(seq1):
-        observations.append(0)
+        observations.append('D')
         i += 1
     while j < len(seq2):
-        observations.append(0)
+        observations.append('D')
         j += 1
 
     return observations
